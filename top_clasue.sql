268c036b32820388b9ec8974915fef6ee47be8ec
SainiAashish11 <aashishkhandelwal1570@gmail.com>
2026-03-09 01:25:53 +0530

    top clause used to select the given number of top rows from the final table

---
+++ top_clasue.sql
@@ -0,0 +1,5 @@
+-- 'top' clause : used for selecting specific number of rows in a table
+
+select top 3*     -- star(*) is needed to specify for how many columns are needed in 3 rows
+
+from customers
