fb854e21a87663e11c2530eee217aa5254b13d2b
SainiAashish11 <aashishkhandelwal1570@gmail.com>
2026-03-11 01:22:38 +0530

    remove all the rows but remains the table structure as it is

---
+++ truncate_clause.sql
@@ -0,0 +1,7 @@
+-- delete all data from the table persons
+
+truncate table persons      -- after truncation, data is deleted but table still eixts with the same structure
+
+select * from persons
+
+-- truncate clause doesn't support 'where' clause which only deletes specific row data
