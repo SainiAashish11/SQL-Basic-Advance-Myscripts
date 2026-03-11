184e9f70589f7abd1b50744aa22165c8e43f9fbd
SainiAashish11 <aashishkhandelwal1570@gmail.com>
2026-03-11 12:05:40 +0530

    Comparison operators are used to compute queries on the basis of Mathematical relational operators

---
+++ comparison_operators.sql
@@ -0,0 +1,25 @@
+-- comparison operators
+
+select *
+from customers 
+where score = 500
+
+select *
+from customers 
+where score != 500     --   != is also written as <>
+
+select *
+from customers
+where score > 500
+
+select *
+from customers
+where score >= 500
+
+select *
+from customers
+where score < 500
+
+select *
+from customers
+where score <= 500
