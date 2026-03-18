e5e4f7b2f8d4d84a6b7c403ffc3bc80b022c6a8c
SainiAashish11 <aashishkhandelwal1570@gmail.com>
2026-03-18 15:56:49 +0530

    Returns the given round-off value to a decimal value

---
+++ round() function.sql
@@ -0,0 +1,6 @@
+select 
+3.516 as number,     -- static 'int' value without quatation and column name as 'number'
+
+round(3.516, 2) as round_2,
+round(3.516, 1) as round_1,
+round(3.516, 0) as round_0
